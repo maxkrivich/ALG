de4c7be0f77831e0f3f4736d0d77797558a0d0ee
maxkrivich <maxkrivich@gmail.com>
2015-06-29 10:17:54 +0300

    Create Bonus_cycles.dpr

---
+++ First/Bonus_cycles.dpr
@@ -0,0 +1,21 @@
+program Bonus_cycles;
+{$APPTYPE CONSOLE}
+uses SysUtils;
+
+var
+  a, b, c, sq, n: integer;
+
+begin
+  write('Input "N": ');
+  Readln(n);
+  if n >= 5 then
+    for a := 1 to n do
+      for b := 1 to n do
+      begin
+        sq := sqr(a) + sqr(b);
+        c := Trunc(Sqrt(sq));
+        if (sqr(c) = sq) and (c <= n) then
+          Writeln(' > ', 'sqr(', a, ') + sqr(', b, ') = sqr(', c, ')');
+      end;
+  Readln;
+end.
